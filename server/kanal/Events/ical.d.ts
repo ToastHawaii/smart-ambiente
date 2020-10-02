802aa8051eabeb13a130e0ead910543287e1a0ae
ToastHawaii <mar.pel@protonmail.com>
2020-10-02 21:11:17 +0200

    Add license

--- server/kanal/Events/ical.d.ts
+++ server/kanal/Events/ical.d.ts
@@ -1,1 +1,18 @@
+// Copyright (C) 2020 Markus Peloso
+// 
+// This file is part of smart-ambiente.
+// 
+// smart-ambiente is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+// 
+// smart-ambiente is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+// 
+// You should have received a copy of the GNU Affero General Public License
+// along with smart-ambiente.  If not, see <http://www.gnu.org/licenses/>.
+
 declare module "ical";
